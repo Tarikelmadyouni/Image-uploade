bb459c97f362d02fdb099700b59b0f34cb078c16
jedafinal <jedafinal@hotmail.com>
2020-01-27 13:32:05 +0100

    teste image"

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+
+        <div  class ="row justify-content-center mb-3" ref="imageUpload"></div>
+
+            <div class="col-12 bg-dark text-white rounded py-3 my-2 text-center">DROPE IMAGE(S) HERE</div>       
+    </div>
+</template>
+
+<script>
+
+     import Dropezone from 'dropzone'; 
+
+    export default {
+
+             data:function(){
+
+                 return{
+
+                    dropzone:null 
+
+                 }
+
+             },        
+        mounted() {
+             
+               this.dropzone = new Dropezone(this.$refs.imageUpload, {
+                      
+                          url: '/images'
+               });
+        }
+    }
+</script>
